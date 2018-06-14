91e44a4c7c660f7e487d6a1132ef0ca3f2b842f7
Alex Baker <alex37454@gmail.com>
2018-06-13 20:44:40 -0500

    small changes forgotten

--- src/main/java/com/amazonaws/lambda/studytime/ShuffleOnIntentHandler.java
+++ src/main/java/com/amazonaws/lambda/studytime/ShuffleOnIntentHandler.java
@@ -8,26 +8,26 @@ import com.amazon.ask.dispatcher.request.handler.RequestHandler;
 import com.amazon.ask.model.Response;
 import com.amazon.ask.request.Predicates;
 import com.amazonaws.lambda.studytime.util.State;
- 
+
 public class ShuffleOnIntentHandler implements RequestHandler {
- 
-     @Override
-     public boolean canHandle(HandlerInput input) {
-    	 return input.matches(Predicates.intentName("AMAZON.SuffleOnIntent"));
-     }
- 
-     @Override
-     public Optional<Response> handle(HandlerInput input) {
-    	 Map<String, Object> sessionAttributes = input.getAttributesManager().getSessionAttributes();
- 		State state = new State(sessionAttributes);
- 		state.shuffle();
- 		String next = state.getNextQuestion();
- 		state.save(sessionAttributes);
-         return input.getResponseBuilder()
-                 .withSpeech("The cards have been shuffled. "+next)
-                 .withReprompt(next)
-                 .withShouldEndSession(false)
-                 .build();
-     }
- 
+
+	@Override
+	public boolean canHandle(HandlerInput input) {
+		return input.matches(Predicates.intentName("AMAZON.SuffleOnIntent"));
+	}
+
+	@Override
+	public Optional<Response> handle(HandlerInput input) {
+		Map<String, Object> sessionAttributes = input.getAttributesManager().getSessionAttributes();
+		State state = new State(sessionAttributes);
+		state.shuffle();
+		String next = state.getNextQuestion();
+		state.save(sessionAttributes);
+		return input.getResponseBuilder()
+				.withSpeech("The cards have been shuffled. "+next)
+				.withReprompt(next)
+				.withShouldEndSession(false)
+				.build();
+	}
+
 }
